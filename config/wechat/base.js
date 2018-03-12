d606054bb485e9b131f51a1b20d7d17a718d5812
godbmw <xiaochiyijiu@163.com>
2018-03-12 19:28:55 +0800

    重新配置环境

--- config/wechat/base.js
+++ config/wechat/base.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
-    appID:'wxb52208064f34b7ec',
-    appSecret:'177ee2879bc18035f5ffc248494bf9dd',
-    token:'godbmw123',
+    appID:'',
+    appSecret:'',
+    token:'',
     port:8888
 }
